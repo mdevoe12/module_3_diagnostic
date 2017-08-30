f5363a68fa110483d8c94e3eb7eacd597b8f3ba9
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-30 10:07:44 -0600

    pulling proper data

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,12 +4,8 @@ class Station
   def self.find_by_zip(zip)
     @conn = Faraday.new(url: 'https://api.data.gov')
 
-    # @conn = Faraday.new(url: 'https://api.data.gov/nrel/alt-fuel-stations/v1.json') do |faraday|
-    #   faraday.headers["X-API-KEY"] = "ffvu6ZpDohHmr1ZgvrlGeS6n3lEBOIMDnQzQAABi"
-    #   faraday.adapter Faraday.default_adapter
-    # end
+    result = @conn.get("/nrel/alt-fuel-stations/v1/nearest.json?location=80202&limit=10&api_key=ffvu6ZpDohHmr1ZgvrlGeS6n3lEBOIMDnQzQAABi")
 
-    # @conn.get("/api/alt-fuel-stations/v1/nearest.json?#{zip}")
     binding.pry
   end
 
