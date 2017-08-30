7e3592d356134b226b54cc4dcd902be0b03f99ee
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-30 10:47:33 -0600

    added search param of radius = 6.0 (miles)

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -5,7 +5,7 @@ class NrelService
   end
 
   def get_url(params)
-    response = @conn.get("/nrel/alt-fuel-stations/v1/nearest.json?location=#{params}&limit=10&api_key=#{ENV['nrel_key']}")
+    response = @conn.get("/nrel/alt-fuel-stations/v1/nearest.json?location=#{params}&radius=6.0&limit=10&api_key=#{ENV['nrel_key']}")
     JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
   end
 
