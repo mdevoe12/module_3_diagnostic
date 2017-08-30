8bea509c24f65de622c083c7ef040e2fc2a8b820
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-30 10:58:38 -0600

    updating feature test

--- spec/features/user_searches_alt_fuel_by_zip_spec.rb
+++ spec/features/user_searches_alt_fuel_by_zip_spec.rb
@@ -7,18 +7,16 @@ RSpec.feature "user sees electric and propane sources" do
     visit root_path
 
     expect(page).to have_content("AltFuelFinder")
+    VCR.use_cassette("features/user_searches_alt_fuel_by_zip_spec.rb") do
 
-    # And I fill in the search form with 80203
     fill_in :q, with: "80202"
-    # And I click "Locate"
+
     click_on "Locate"
 
-    # Then I should be on page "/search" with parameters visible in the url
-    # expect(page).to eq("http://localhost:3000/search?utf8=%E2%9C%93&q=80202&commit=Locate")
     expect(page).to have_content("Electric and Propane Stations in your area")
-    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-    
+    expect(page).to have_content(10, "Station:")
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+    end
   end
 end
