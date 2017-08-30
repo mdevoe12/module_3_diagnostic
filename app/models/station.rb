b69c5a12a7ee4db4d94b3c593062e2d9ba20470b
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-30 10:01:06 -0600

    API calls with @conn not giving what I expect, changing @conn

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,10 +2,14 @@ class Station
 
 
   def self.find_by_zip(zip)
-    @conn = Faraday.new(url: 'https://api.data.gov') do |faraday|
-      faraday.headers 'X-Api-Key: ffvu6ZpDohHmr1ZgvrlGeS6n3lEBOIMDnQzQAABi'
-      faraday.adapater Faraday.default_adapter
-    end
+    @conn = Faraday.new(url: 'https://api.data.gov')
+
+    # @conn = Faraday.new(url: 'https://api.data.gov/nrel/alt-fuel-stations/v1.json') do |faraday|
+    #   faraday.headers["X-API-KEY"] = "ffvu6ZpDohHmr1ZgvrlGeS6n3lEBOIMDnQzQAABi"
+    #   faraday.adapter Faraday.default_adapter
+    # end
+
+    # @conn.get("/api/alt-fuel-stations/v1/nearest.json?#{zip}")
     binding.pry
   end
 
