46ce9b26b15de088dc3276831a38c3a2ebddadcc
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-30 10:14:58 -0600

    starting to parse JSON

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,8 +4,9 @@ class Station
   def self.find_by_zip(zip)
     @conn = Faraday.new(url: 'https://api.data.gov')
 
-    result = @conn.get("/nrel/alt-fuel-stations/v1/nearest.json?location=80202&limit=10&api_key=ffvu6ZpDohHmr1ZgvrlGeS6n3lEBOIMDnQzQAABi")
+    response = @conn.get("/nrel/alt-fuel-stations/v1/nearest.json?location=80202&limit=10&api_key=ffvu6ZpDohHmr1ZgvrlGeS6n3lEBOIMDnQzQAABi")
 
+    result = JSON.parse(response.body)
     binding.pry
   end
 
